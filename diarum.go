b8ef9ad6671a17bc51fb6b4a6b4886998a6dfc86
石渠清心 <Felix2yu@users.noreply.github.com>
2026-06-22 20:12:38 +0800

    chore: release v1.8

--- diarum.go
+++ diarum.go
@@ -2,7 +2,7 @@ package main
 
 var (
 	// Version is the current version of Diarum, injected at build time
-	Version = "v1.7"
+	Version = "v1.8"
 	// Name is the application name
 	Name = "diarum"
 )
