8c7fd548e3dfc86aa9c75f20d2d5f542bc57858d
石渠清心 <Felix2yu@users.noreply.github.com>
2026-06-25 20:45:41 +0800

    fix: use Any instead of GET for SPA catch-all to fix Echo v5 routing

--- main.go
+++ main.go
@@ -232,7 +232,8 @@ func run(args []string, stdout io.Writer) error {
 	if err != nil {
 		log.Printf("Warning: Failed to get embedded static files: %v", err)
 	} else {
-		e.GET("/*", func(c echo.Context) error { return serveSPA(c, staticFS) })
+		spaHandler := func(c echo.Context) error { return serveSPA(c, staticFS) }
+		e.Any("/*", spaHandler)
 	}
 
 	if err := startServer(e, *httpAddr); err != nil && !errors.Is(err, http.ErrServerClosed) {
